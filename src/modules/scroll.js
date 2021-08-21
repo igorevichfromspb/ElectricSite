d494c0b8a4253ddbddae1d36c795439b0c6119a5
Ivan Matveev <igorevichwork@gmail.com>
2021-08-21 17:52:59 +0300

    smoothScroll added

--- src/modules/scroll.js
+++ src/modules/scroll.js
@@ -6,7 +6,7 @@ const scroll = () => {
     style.textContent = `
         .up {
           display: none;
-        }  
+        }
         .up-active {
           display: block !important;
         }
@@ -31,8 +31,25 @@ const scroll = () => {
       window.scrollTo(window.pageYOffset, 0);
     }
   };
-
-  up.addEventListener("click", goUp);
+  const smoothScroll = (event) => {
+    const target = event.target;
+    if (target.tagName === "A" && target.parentNode.tagName === "LI") {
+      const id = target.getAttribute("href");
+      event.preventDefault();
+      document.querySelector(id).scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    }
+  };
+  document.addEventListener("click", smoothScroll);
+  up.addEventListener("click", (event) => {
+    event.preventDefault();
+    document.querySelector("body").scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
   document.addEventListener("scroll", addClass);
 };
 export default scroll;
